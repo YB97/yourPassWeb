00adf89c95c963c23249211f45ab9f628644d152
Aleksandr Abanin <abaninsash@gmail.com>
2019-10-03 09:37:23 +0300

    som

--- app/js/common.js
+++ app/js/common.js
@@ -1,13 +1,18 @@
-$(function() {
-  var lastSlideId = $(".js-accordion-button").on("click", function(e) {
+$(function () {
+  var lastSlideId = null
+  $(".js-accordion-button").on("click", function (e) {
     var id = e.target.dataset.count;
     var contentBlock = $('.js-accordion-content[data-count="' + id + '"]');
     if (lastSlideId !== id) {
       $(".js-accordion-content").slideUp("fast");
       contentBlock.slideDown("fast");
       $(
-        '.js-accordion-button[data-count="' + id + '"] span:last-child'
+        '.js-accordion-button span:last-child'
       ).removeClass("faq-arrow--rotate");
+      $(
+        '.js-accordion-button[data-count="' + id + '"] span:last-child'
+      ).toggleClass("faq-arrow--rotate");
+
     } else {
       contentBlock.slideToggle("fast");
       $(
@@ -16,8 +21,15 @@ $(function() {
     }
     lastSlideId = id;
   });
+  $('.hamburger').on('click', function (event) {
+    event.stopPropagation();
+    $('.hamburger').toggleClass('is-active');
+    $('.nav').toggleClass('darken')
+    $('.nav-list').slideToggle('fast')
+  })
 });
-$(function() {
+
+$(function () {
   var element_form1 = document.getElementById("phone-input");
   var maskOptions = {
     mask: "+{7}(000)000-00-00"
@@ -29,4 +41,26 @@ $(function() {
 
   var element_form3 = document.getElementById("request-phone-num");
   var mask3 = IMask(element_form3, maskOptions);
+});
+
+
+$(document).ready(function () {
+
+  //E-mail Ajax Send
+  $("form").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+
 });
